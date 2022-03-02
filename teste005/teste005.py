8dc9fef63e7a33ad2657f1057de27c7d27a4057f
carloseduardolannes <carloseduardo.lannes@gmail.com>
2022-03-02 19:52:32 -0300

    teste005

--- teste005/teste005.py
+++ teste005/teste005.py
@@ -6,26 +6,25 @@ def front(s,t):
     print('=-='*11)
     sleep(t)
     print(f'Step {s}:')
-    return None
 
 def screen(teste,passo):
-    driver.save_screenshot(f'teste002\{teste}_passo_{passo}.png')
+    driver.save_screenshot(f'teste005\{teste}_passo_{passo}.png')
     return None        
 
-print('=-='*2,'Realizando teste002','=-='*2)
+print('=-='*2,'Realizando teste005','=-='*2)
 
 while(True):
     
     t=0
-    teste = "teste002_cria_novo_time_"
+    teste = "teste004_entrar_time"
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(options=options)
-
+    
     ##### Step 1 #####
     
     passo = 1.0
-    front(passo,t) 
+    front(passo,t)  
     print('''        Acessar a url: https://escritorioagil.netlify.app/
         ''')
     driver.get('https://escritorioagil.netlify.app/')
@@ -53,54 +52,48 @@ while(True):
     else:
         print(f'Não clicou!')
         break
+    
     print('''        Completando os campos: 
         Email e ''',end="")
-    driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[2]/input').send_keys('teste@gmail.com')
+    driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[2]/input').send_keys('carlos@uff.br')
     sleep(t)
     print('Senha.')
     driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[3]/input').send_keys('1234567')
+    sleep(t)
     screen(teste,passo)
-    sleep(0.5)
     print('Clicando no Botão entrar') 
     for i in range(1,4):
         print(' * ',end="")      
     driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[4]/button').click()
-    sleep(2)
-    if driver.current_url == 'https://escritorioagil.netlify.app/teams':
+    sleep(1)
+    if driver.current_url == 'https://escritorioagil.netlify.app/signin':
         print("Entrou!\n")
         sleep(t)
     else:
         print('Não acessou!')
         break
+    sleep(1)
+    passo = 2.1
+    screen(teste,passo)
     
     ##### Step 3 #####
     
     passo = 3.0
-    front(passo,t)
-    print('''        Clicando em Adicionar''')
-    driver.find_element(by=By.XPATH, value='/html/body/div/div/div[4]/div[1]/button').click()
-    sleep(t)
-    if driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/input'):
-        print("Clicou e entrou!\n")
-        sleep(t)
-    else:
-        print('Não entrou!')
-        break
-    
-    ##### Step 4 #####
-    
-    passo = 4.0
-    front(passo,t)
-    print('''        Cadastrando um novo time''')
-    driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/input').send_keys('Time dos Sonhos')
-    driver.find_element_by_xpath("//div[@class='actions']/a").click()
-    
-    print('=-='*11)
-    sleep(0.5)
+    front(passo,t) 
+    driver.find_element(by=By.XPATH, value='/html/body/div/div/div[3]/div/button').click()
+    sleep(1)
+    driver.find_element(by=By.XPATH, value="//input[@placeholder='Código Ex: SW52aX...']").send_keys('SW52aXRhdGlvbi00MDU=')
+    sleep(1)
+    passo = 3.1
     screen(teste,passo)
-    print('Teste finalizado sem nenhum erro!')    
+    driver.find_element(by=By.XPATH, value='/html/body/div/div/div[1]/div[2]/div[2]/div/button').click()
+    sleep(1)
+    passo = 3.2
+    screen(teste,passo)
+    print('=-='*11)
+    print('Teste finalizado sem nenhum erro!')
     
-    #driver.close()
-    #driver.quit()   
-     
+    break
+    driver.close()
+    driver.quit()
     break
