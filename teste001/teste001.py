b62d68e6c6b5cd7dead7cb64433f352b2d0443c1
carloseduardolannes <carloseduardo.lannes@gmail.com>
2022-03-03 10:37:55 -0300

    limpeza do codigo de teste 1 e teste 2

--- teste001/teste001.py
+++ teste001/teste001.py
@@ -2,93 +2,49 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
-def front(s,t):
-    print('=-='*11)
-    sleep(t)
-    print(f'Step {s}:')
-    return None
-
 def screen(teste,passo):
     driver.save_screenshot(f'teste001\{teste}_passo_{passo}.png')
-    return None        
+    return None
 
-print('=-='*2,'Realizando teste001','=-='*2)
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
+driver.maximize_window() 
 
 while(True):
+    
     t=0
-    teste = "teste001_acesso_url_"
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(options=options)
+    teste = "teste001_acesso_url_"   
     
     ##### Step 1 #####
-    
-    passo = 1.0
-    front(passo,t)  
-    print('''        Acessar a url: https://escritorioagil.netlify.app/
-        ''')
+    passo = 1.0  
+    #Acessando a url: https://escritorioagil.netlify.app/
     driver.get('https://escritorioagil.netlify.app/')
-    print('Verificando acesso a url:') 
-    if driver.current_url == 'https://escritorioagil.netlify.app/':
-        print("Acessou a url: https://escritorioagil.netlify.app/\n")
-        sleep(t)
-        screen(teste,passo)    
-    else:
-        print('Não acessou a url: https://escritorioagil.netlify.app/\n')
-        break
+    screen(teste,passo)
     
     ##### Step 2 #####
-    
-    passo = 2.0
-    front(passo,t) 
-    print('''        Clicando em acessar conta''')
+    passo = 2.0 
+    #Clicando em acessar conta
     driver.find_element(by=By.PARTIAL_LINK_TEXT, value='Log in').click()
-    for i in range(1,4):
-        print(' * ',end="")
-        sleep(t)
-    if driver.current_url == 'https://escritorioagil.netlify.app/signin':
-        print("Clicou!\n")
-        sleep(t)
-    else:
-        print(f'Não clicou!')
-        break
-
-    print('''        Completando os campos: 
-        Email e ''',end="")
+    #Completando os campos: Email e senha.
     driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[2]/input').send_keys('teste@gmail.com')
-    sleep(t)
-    print('Senha.')
     driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[3]/input').send_keys('1234567')
-    sleep(t)
     screen(teste,passo)
-    print('Clicando no Botão entrar') 
-    for i in range(1,4):
-        print(' * ',end="")      
+    #Clicando no Botão entrar    
     driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[4]/button').click()
     sleep(1)
     
     ##### Step 3 #####
-    
     passo = 3.0
     sleep(1)
-    if driver.current_url == 'https://escritorioagil.netlify.app/teams':
-        print("Entrou!\n")
-        screen(teste,passo)
-        sleep(t)
-    else:
-        print('Não acessou!')
-        break
+    screen(teste,passo)
     sleep(0.5)
     passo = 4
     driver.find_element(by=By.XPATH, value='/html/body/div/div/div[2]/a[2]').click()
     passo = 3.1
     sleep(0.5)
     screen(teste,passo)
-    print('=-='*11)
-    print('Teste finalizado sem nenhum erro!')
-    sleep(t)
-    
+    #Teste finalizado sem nenhum erro!
     driver.close()
-    driver.quit()   
-     
+    driver.quit()  
     break
--- teste002/teste002.py
+++ teste002/teste002.py
@@ -2,105 +2,62 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
-def front(s,t):
-    print('=-='*11)
-    sleep(t)
-    print(f'Step {s}:')
-    return None
-
 def screen(teste,passo):
     driver.save_screenshot(f'teste002\{teste}_passo_{passo}.png')
     return None        
 
-print('=-='*2,'Realizando teste002','=-='*2)
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
+driver.maximize_window() 
 
 while(True):
     
     t=0
     teste = "teste002_cria_novo_time_"
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(options=options)
 
     ##### Step 1 #####
     
-    passo = 1.0
-    front(passo,t) 
-    print('''        Acessar a url: https://escritorioagil.netlify.app/
-        ''')
+    passo = 1.0 
+    #Acessar a url: https://escritorioagil.netlify.app/
     driver.get('https://escritorioagil.netlify.app/')
     print('Verificando acesso a url:')
     screen(teste,passo)
-    if driver.current_url == 'https://escritorioagil.netlify.app/':
-        print("Acessou a url: https://escritorioagil.netlify.app/\n")
-        sleep(t)
-    else:
-        print('Não acessou a url: https://escritorioagil.netlify.app/\n')
-        break
+    sleep(t)
+
     
     ##### Step 2 #####
     
-    passo = 2.0
-    front(passo,t) 
-    print('''        Clicando em acessar conta''')
+    passo = 2.0 
+    #Clicando em acessar conta
     driver.find_element(by=By.PARTIAL_LINK_TEXT, value='Log in').click()
-    for i in range(1,4):
-        print(' * ',end="")
-        sleep(t)
-    if driver.current_url == 'https://escritorioagil.netlify.app/signin':
-        print("Clicou!\n")
-        sleep(t)
-    else:
-        print(f'Não clicou!')
-        break
-    print('''        Completando os campos: 
-        Email e ''',end="")
+    sleep(t)
+    #Completando os campos: Email e Senha.
     driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[2]/input').send_keys('teste@gmail.com')
     sleep(t)
-    print('Senha.')
     driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[3]/input').send_keys('1234567')
     screen(teste,passo)
     sleep(0.5)
-    print('Clicando no Botão entrar') 
-    for i in range(1,4):
-        print(' * ',end="")      
+    #Clicando no Botão entrar      
     driver.find_element(by=By.XPATH, value='/html/body/div/div/form/div[4]/button').click()
     sleep(2)
-    if driver.current_url == 'https://escritorioagil.netlify.app/teams':
-        print("Entrou!\n")
-        sleep(t)
-    else:
-        print('Não acessou!')
-        break
-    
+        
     ##### Step 3 #####
-    
     passo = 3.0
-    front(passo,t)
-    print('''        Clicando em Adicionar''')
+    #Clicando em Adicionar
     driver.find_element(by=By.XPATH, value='/html/body/div/div/div[4]/div[1]/button').click()
     sleep(t)
-    if driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/input'):
-        print("Clicou e entrou!\n")
-        sleep(t)
-    else:
-        print('Não entrou!')
-        break
+    
     
     ##### Step 4 #####
     
     passo = 4.0
-    front(passo,t)
-    print('''        Cadastrando um novo time''')
+    #Cadastrando um novo time
     driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/input').send_keys('Time dos Sonhos')
     driver.find_element_by_xpath("//div[@class='actions']/a").click()
     
-    print('=-='*11)
     sleep(0.5)
     screen(teste,passo)
-    print('Teste finalizado sem nenhum erro!')    
-    
-    #driver.close()
-    #driver.quit()   
-     
+    driver.close()
+    driver.quit()
     break
